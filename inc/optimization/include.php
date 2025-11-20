a83b4e11aa93e2cb83abdd5d4164d16cceb0d6ed
Marko Dimitrijevic <mdimitrijevic84@gmail.com>
2025-11-20 13:02:58 +0100

    Refactor speculation rules filter.

--- inc/optimization/include.php
+++ inc/optimization/include.php
@@ -302,12 +302,15 @@ if ( ! function_exists( 'elektromikron_modify_speculation_rules' ) ) {
 	add_filter( 'wp_speculation_rules_configuration', 'elektromikron_modify_speculation_rules' );
 }
 
-// Prevent speculative loading of specific link
 add_filter(
 	'wp_speculation_rules_href_exclude_paths',
 	function ( $href_exclude_paths, $mode ) {
 		if ( 'prerender' === $mode ) {
+			// Version without the trailing slash (your original).
 			$href_exclude_paths[] = '/pneumatski-uredjaji/ventil-upravljacki-komandna-slavina';
+
+			// Version WITH the trailing slash (NEW).
+			$href_exclude_paths[] = '/pneumatski-uredjaji/ventil-upravljacki-komandna-slavina/';
 		}
 		return $href_exclude_paths;
 	},
